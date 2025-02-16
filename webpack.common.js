bce5b92c4c6d70f2219cd2a66f4ed03affcd3b52
Darmanto97 <darmantodana8@gmail.com>
2025-02-16 09:19:47 +0700

    update
    noteapp update

---
+++ webpack.common.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'index.html'),
+            filename: 'index.html',
+        }),
+
+    new CopyWebpackPlugin ({
+            patterns: [
+                {
+                    from: path.join(__dirname, 'src/public/notes-image.jpg'),
+                    to: path.join(__dirname, 'dist'),
+                },
+            ],
+        }),
+    ],
+};
---
+++ webpack.prod.js
@@ -0,0 +1,23 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const { default: test } = require('node:test');
+
+module.exports = merge(common, {
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+});
